d8f721b1b852b8a4cdca6f08715182f6e25a1897
johnbosco muiruri <johnmuiruri352@gmail.com>
2023-01-21 17:12:05 -0600

    first commit

---
+++ calculator.js
@@ -0,0 +1,16 @@
+//Calculator Setup
+
+
+// Variables 
+const ins = num => input1.textContent += num;
+const Refresh = () => input1.textContent = " ";
+const equal = () => input1.textContent = eval(input1.textContent).toFixed(7);
+
+var input1 = document.getElementById('input1');
+
+
+
+
+
+
+
